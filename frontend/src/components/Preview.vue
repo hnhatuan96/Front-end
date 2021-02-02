fd435a5eb131db4dbf2534facc98cdf6120abb16
Tuan Huynh Nha <tuan.huynh@3forcom.com>
2021-02-02 11:51:52 +0700

    :sparkles: Done Simple Applcation

---
+++ frontend/src/components/Preview.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="preview-second">
+    <p class="preview-second_mess">{{ msg }}</p>
+    <button
+      class="preview-second_btn"
+      v-for="(modal,index) in dataModal"
+      :key="index"
+      @click="onHandlePopup($refs, index)"
+    >
+      Open modal {{index + 1}}
+    </button>
+
+    <modal
+      ref="modalName"
+      v-bind:data-header="dataPreviewModal.dataHeader"
+      v-bind:data-body="dataPreviewModal.dataBody"
+      v-bind:data-footer="dataPreviewModal.dataFooter"
+    />
+  </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+
+export default {
+  name: 'Preview',
+  data () {
+    return {
+      msg: 'Welcome to My Application',
+      dataPreviewModal: {
+        dataHeader: '',
+        dataBody: '',
+        dataFooter: ''
+      },
+      dataModal: [
+        {
+          dataHeader: 'This is Header First',
+          dataBody: 'This is Body First',
+          dataFooter: 'This is Footer First'
+        },
+        {
+          dataHeader: 'This is Header Second',
+          dataBody: 'This is Body Second',
+          dataFooter: 'This is Footer Second'
+        },
+        {
+          dataHeader: 'This is Header Third',
+          dataBody: 'This is Body Third',
+          dataFooter: 'This is Footer Third'
+        }
+      ]
+    }
+  },
+  components: {
+    Modal
+  },
+  methods: {
+    onHandlePopup (ref, position) {
+      this.dataPreviewModal = this.dataModal[position]
+      ref.modalName.openModal()
+    }
+  }
+}
+</script>
+
+<style>
+.preview-second {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.preview-second_mess {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.preview-second_btn {
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.overflow-hidden {
+  overflow: hidden;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import View from '@/components/View'
-// import Modal from '@/components/Modal'
+import Preview from '@/components/Preview'
 
 Vue.use(Router)
 
@@ -10,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/view',
-      name: 'View',
-      component: View
+      name: 'Preview',
+      component: Preview
     }
   ]
 })
